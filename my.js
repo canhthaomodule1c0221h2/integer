8d2d5000b3d57f005c44d60cc7d9e73c1fdea16d
canhthaomodule1c0221h2 <79823013+canhthaomodule1c0221h2@users.noreply.github.com>
2021-04-09 10:36:08 +0700

    Add files via upload

---
+++ my.js
@@ -0,0 +1,7 @@
+let integer = parseInt(prompt("nhập số vào"))
+if (integer >0) {
+    document.write( + integer + " là số lớn hơn không " );
+}
+else {
+    document.write(integer + " là số bé hơn không ");
+}
